58ec231b0c29d47828244f133c328e5fdfe5568a
Santhoshhl <santhoshhl@live.in>
2019-04-10 05:02:58 +0530

    Add files via upload

---
+++ Webscraping.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+
+url='https://www.infibeam.com/Mobiles/search?mobiletype=smart-phone'
+
+res=requests.get(url).text
+result=BeautifulSoup(res,"lxml")
+
+
+
+with open('infibeam.csv','w',newline='') as f:
+    f=csv.writer(f)
+    f.writerow(['Title','Url'])
+    for title in result.find_all("div",class_="title"):
+        get_title=title.a.text
+            
+        url=title.a.get("href")
+        url='https://www.infibeam.com'+url
+        
+        
+        print(get_title)
+        print(url)
+        f.writerow([get_title,url])
+        
+            
+        
+      
+        
+                
+    
+    
+
+
